2489da78cb838fa07e6bffca6a994bf16a9a6e06
misrtrkladiva <roman.pausch@seznam.cz>
2025-11-23 11:42:39 +0100

    Přidejte soubory projektu.

---
+++ LekceSprite.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "game");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        sf::RectangleShape rect(sf::Vector2f(100.f, 100.f));
+        rect.setFillColor(sf::Color::Red);
+        rect.setPosition(sf::Vector2f(100.f, 100.f));
+
+        window.clear(sf::Color::Black);
+        window.draw(rect);
+
+        window.display();
+    }
+}
